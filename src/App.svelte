9de03a1986deedb6733b1954139bb7f5884e985d
Carlos Solorzano <carlos.ace27@gmail.com>
2023-06-26 19:40:59 -0600

    feat: :sparkles: Add propotype design card

--- src/App.svelte
+++ src/App.svelte
@@ -1,39 +1,25 @@
 <script lang="ts">
-  import Greet from './lib/Greet.svelte'
+  import Card from "./Play/Card.svelte";
+  const containerStyle =
+    "flex flex-col min-h-screen items-center justify-center mx-auto px-2";
 </script>
 
-<main class="container">
-  <h1>Welcome to Tauri!</h1>
-
-  <div class="row">
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo vite" alt="Vite Logo" />
-    </a>
-    <a href="https://tauri.app" target="_blank">
-      <img src="/tauri.svg" class="logo tauri" alt="Tauri Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank">
-      <img src="/svelte.svg" class="logo svelte" alt="Svelte Logo" />
-    </a>
+<main class={containerStyle}>
+  <h1 class="font-bold text-2xl">Memory Game</h1>
+  <br />
+  <div class="flex flex-wrap gap-4 justify-center  lg:px-6">
+    {#each { length: 24 } as _, i}
+      <Card />
+    {/each}
   </div>
-
-  <p>
-    Click on the Tauri, Vite, and Svelte logos to learn more.
-  </p>
-
-  <div class="row">
-    <Greet />
-  </div>
-
-
 </main>
 
 <style>
-  .logo.vite:hover {
-    filter: drop-shadow(0 0 2em #747bff);
-  }
+  /* .card:hover {
+    filter: drop-shadow(0 0 0.4em #747bff);
+  } */
 
-  .logo.svelte:hover {
+  /* .logo.svelte:hover {
     filter: drop-shadow(0 0 2em #ff3e00);
-  }
+  } */
 </style>
